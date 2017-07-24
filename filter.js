e590ffea1c89350033491fb8d72e4a67ba43789d
zitongliu <zitongliu7@gmail.com>
2017-07-24 10:59:37 +1000

    updated filter example

--- filter.js
+++ filter.js
@@ -1,8 +1,9 @@
 const products = [
-  { name: 'cucumber', type: 'vegetable' },
-  { name: 'banana', type: 'fruit' },
-  { name: 'celery', type: 'vegetable' },
-  { name: 'orange', type: 'fruit' }
+  { name: 'cucumber', type: 'vegetable', quantity: 0, price: 1 },
+  { name: 'potatoes', type: 'vegetable', quantity: 89, price: 4 },
+  { name: 'banana', type: 'fruit', quantity: 10, price: 15 },
+  { name: 'celery', type: 'vegetable', quantity: 30, price: 13 },
+  { name: 'orange', type: 'fruit', quantity: 3, price: 5 }
 ];
 
 // if we want to see all the fruits
@@ -31,3 +32,16 @@ console.log(filteredProductsNew);
 filteredProductsNew = products.filter(function(product){
   return product.type === 'vegetable';
 });
+
+// filterd that only keep vegetable with quantity greater than 0 and less than 10
+
+filteredProducts = products.filter(function(product){
+  return (
+    product.type === 'vegetable' &&
+    product.quantity > 0 &&
+    product.price <= 10
+  );
+});
+
+console.log('vegetables in stock that is 10 dollars or less');
+console.log(filteredProducts)
